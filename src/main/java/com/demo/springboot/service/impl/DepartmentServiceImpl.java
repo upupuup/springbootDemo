0396141b1773b23c22d161106e6f0a9db0699f59
upupuup <woyaojiayou_a@163.com>
2019-01-27 21:03:27 +0800

    新增DepartServiceImpl类

---
+++ src/main/java/com/demo/springboot/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,18 @@
+package com.demo.springboot.service.impl;
+
+import com.demo.springboot.bean.Department;
+import com.demo.springboot.bean.Employee;
+import com.demo.springboot.mapper.DepartmentMapper;
+import com.demo.springboot.service.IDepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("iDepartmentService")
+public class DepartmentServiceImpl implements IDepartmentService {
+    @Autowired
+    private DepartmentMapper departmentMapper;
+
+    public Department getDeptById(Integer id) {
+        return departmentMapper.getDeptById(id);
+    }
+}
