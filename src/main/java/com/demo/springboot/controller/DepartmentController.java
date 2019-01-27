bdee12c4b737df90a39ef44710ae43dbb5aff6ec
upupuup <woyaojiayou_a@163.com>
2019-01-27 21:01:22 +0800

    使用RequestMapping、ResponseBody注解

--- src/main/java/com/demo/springboot/controller/DepartmentController.java
+++ src/main/java/com/demo/springboot/controller/DepartmentController.java
@@ -1,14 +1,30 @@
 package com.demo.springboot.controller;
 
 import com.demo.springboot.bean.Department;
+import com.demo.springboot.service.IDepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class DepartmentController {
 
-//    public int insertDept(Department department) {
-//
-//    }
+    @Autowired
+    private IDepartmentService iDepartmentService;
 
-//    public Department getDeptById(Integer id) {
-//
-//    }
+    @RequestMapping("/dept/insertDept")
+    public int insertDept(Department department) {
+        return 1;
+    }
+
+    // @RequestMapping("/dept/{id}")
+    // @GetMapping("/dept/{id}")
+    // public Department getDeptById(@PathVariable(value = "id") Integer id) {
+        // return iDepartmentService.getDeptById(id);
+    // }
+    @RequestMapping("/dept/getDeptById")
+    @ResponseBody
+    public Department getDeptById(Integer id) {
+        System.out.println("进入springboot");
+        return iDepartmentService.getDeptById(id);
+    }
 }
